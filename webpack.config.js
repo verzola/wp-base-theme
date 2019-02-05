ababcbd91ec411679c1404d387e98f295ccf553b
Gustavo Verzola <verzola@gmail.com>
2019-02-05 20:18:14 -0200

    create prettier config file and reformat code

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,91 @@
-const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
-const ImageminPlugin = require("imagemin-webpack-plugin").default;
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const StyleLintPlugin = require("stylelint-webpack-plugin");
-const WebpackAssetsManifest = require("webpack-assets-manifest");
-const WebpackBar = require("webpackbar");
-const dev = process.env.NODE_ENV !== "production";
-const path = require("path");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const ImageminPlugin = require('imagemin-webpack-plugin').default
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const StyleLintPlugin = require('stylelint-webpack-plugin')
+const WebpackAssetsManifest = require('webpack-assets-manifest')
+const WebpackBar = require('webpackbar')
+const dev = process.env.NODE_ENV !== 'production'
+const path = require('path')
 
 module.exports = {
-  mode: dev ? "development" : "production",
-  devtool: "source-map",
+  mode: dev ? 'development' : 'production',
+  devtool: 'source-map',
   entry: {
-    main: "./src/js/index.js"
+    main: './src/js/index.js',
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: dev ? "[name].js" : "[name].[hash].js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: dev ? '[name].js' : '[name].[hash].js',
   },
   externals: {
-    jquery: "jQuery"
+    jquery: 'jQuery',
   },
   plugins: [
-    new CleanWebpackPlugin(["dist"]),
+    new CleanWebpackPlugin(['dist']),
     new BrowserSyncPlugin({
-      host: "localhost",
+      host: 'localhost',
       port: 3000,
-      proxy: process.env.BROWSERSYNC_PROXY || "http://localhost",
-      files: ["**/*.php"]
+      proxy: process.env.BROWSERSYNC_PROXY || 'http://localhost',
+      files: ['**/*.php'],
     }),
     new MiniCssExtractPlugin({
-      filename: dev ? "[name].css" : "[name].[hash].css",
-      chunkFilename: dev ? "[id].css" : "[id].[hash].css"
+      filename: dev ? '[name].css' : '[name].[hash].css',
+      chunkFilename: dev ? '[id].css' : '[id].[hash].css',
     }),
     new StyleLintPlugin(),
     new FriendlyErrorsWebpackPlugin(),
     new ImageminPlugin({
-      disable: dev
+      disable: dev,
     }),
     new WebpackBar(),
-    new WebpackAssetsManifest()
+    new WebpackAssetsManifest(),
   ],
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader"
+        loader: 'eslint-loader',
       },
       {
         test: /\.m?js$/,
         exclude: /(node_modules)/,
         use: [
           {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"]
-            }
-          }
-        ]
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
       },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
           dev
-            ? "style-loader"
+            ? 'style-loader'
             : {
                 loader: MiniCssExtractPlugin.loader,
                 options: {
-                  publicPath: "./"
-                }
+                  publicPath: './',
+                },
               },
-          "css-loader",
-          "postcss-loader",
-          "sass-loader"
-        ]
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
+        use: ['file-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"]
-      }
-    ]
-  }
-};
+        use: ['file-loader'],
+      },
+    ],
+  },
+}
